bc116486a2bfcf69f14bafa50b2c7822f7f0a6b6
jambrocio-dev17 <sebasambrocio2005@gmail.com>
2026-04-28 22:44:47 -0600

    Se inicializa la estructura que servira para el desarrollo de las demas ramas.

---
+++ sistema-condominio-vista-verde/src/main/java/com/mycompany/sistema/condominio/vista/verde/SistemaCondominioVistaVerde.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema.condominio.vista.verde;
+
+/**
+ *
+ * @author Josue Ambrocio
+ */
+public class SistemaCondominioVistaVerde {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
