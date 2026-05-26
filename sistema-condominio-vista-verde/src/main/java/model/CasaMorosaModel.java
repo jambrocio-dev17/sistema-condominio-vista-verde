a11963e8c0c91316d7a357488fc776b66973517a
hdezurbina08-create <hdez.urbina08@gmail.com>
2026-05-26 17:52:14 -0600

    se finaliza con el modelo para la pantalla de casas morosas

--- sistema-condominio-vista-verde/src/main/java/model/CasaMorosaModel.java
+++ sistema-condominio-vista-verde/src/main/java/model/CasaMorosaModel.java
@@ -9,5 +9,29 @@ package model;
  * @author yeimy
  */
 public class CasaMorosaModel {
-    
+    private int    numeroCasa;
+    private String propietario;
+    private String telefono;
+    private int    mesesDeuda;
+
+    public CasaMorosaModel() {}
+
+    public CasaMorosaModel(int numeroCasa, String propietario, String telefono, int mesesDeuda) {
+        this.numeroCasa  = numeroCasa;
+        this.propietario = propietario;
+        this.telefono    = telefono;
+        this.mesesDeuda  = mesesDeuda;
+    }
+
+    public int    getNumeroCasa()                  { return numeroCasa; }
+    public void   setNumeroCasa(int numeroCasa)    { this.numeroCasa = numeroCasa; }
+
+    public String getPropietario()                 { return propietario; }
+    public void   setPropietario(String p)         { this.propietario = p; }
+
+    public String getTelefono()                    { return telefono; }
+    public void   setTelefono(String t)            { this.telefono = t; }
+
+    public int    getMesesDeuda()                  { return mesesDeuda; }
+    public void   setMesesDeuda(int m)             { this.mesesDeuda = m; }
 }
